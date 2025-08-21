9911d50494745ce3c73a9696d01f184fd74684ff
Kaizel-7274 <adriancisse17@gmail.com>
2025-08-21 08:17:21 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
 
 const App = () => {
   return (
-    <div>
-      <h1>R41Z3L - Order Now </h1>
-      <Order />
-      <footer>
-        <p>© 2023 R41Z3L Pizzeria</p>
-      </footer>
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">R41Z3L - Order Now </h1>
+        <Order />
+        <PizzaOfTheDay />
+        <footer>
+          <p>© 2023 R41Z3L Pizzeria</p>
+        </footer>
+      </div>
+    </StrictMode>
   );
 };
 
